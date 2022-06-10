6d565689ce5d60e442b7ea5edf5fb1223df70afd
Kaipaev Timur <kaipaevtimur55@gmail.com>
2022-06-10 17:50:46 +0200

    added functionality

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -12,9 +12,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var cenliusLabel: UILabel!
     @IBOutlet weak var fahrenheitLabel: UILabel!
     
-    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+        slider.maximumValue = 100
+        slider.minimumValue = 0
+        slider.value = 0
+        }
+    }
     
     @IBAction func sliderChanged(_ sender: UISlider) {
+        let temperatureCenlius = Int(round(slider.value))
+        cenliusLabel.text = "\(temperatureCenlius)ºC"
+        
+        let tetemperatureFahrenheit = Int(round(slider.value * 9 / 5 + 32))
+        fahrenheitLabel.text = "\(tetemperatureFahrenheit)ºF"
+        
     }
     
 }
